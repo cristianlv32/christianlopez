e4057d8c1d95692397affdd1580c64f02884ef0c
Christian Lopez <cristianlv32@gmail.com>
2024-11-19 16:57:16 +0100

    no se permiten numeros negativos y se elimina Ejercicio41.java

--- src/ud4funciones/Ejercicio43.java
+++ src/ud4funciones/Ejercicio43.java
@@ -1,17 +1,22 @@
 package ud4funciones;
 
 public class Ejercicio43 {
-	static void dibujarRectangulo(int ancho, int alto) {
+	static void dibujarRectangulo(int ancho, int alto)
 
-		for (int i = 0; i < alto; i++) {
-			for (int j = 0; j < ancho; j++) {
-				System.out.print("*");
+if (ancho > 0 && alto > 0) {
+			for (int i = 0; i < alto; i++) {
+				for (int j = 0; j < ancho; j++) {
+					System.out.print("*");
+				}
+				System.out.println();
 			}
-			System.out.println();
+		} else {
+			System.out.println("El ancho o el alto son menores que 0");
 		}
 	}
 
 	public static void main(String[] args) {
-		dibujarRectangulo(-5, 8);
+		dibujarRectangulo(10, 8);
+		dibujarRectangulo(-4, 1); // ejemplo números negativos
 	}
 }
